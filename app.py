d26ed2e95941e47a0d7d6baf107277b5f2f7f369
Mert çelikol <74824635+mertcelikol@users.noreply.github.com>
2025-07-31 22:20:31 +0000

    linked list with findmiddle function

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from dataclasses import dataclass
-
 class Node:
     def __init__(self, data):
         self.data = data
         self.next_node = None
 
-@dataclass
-class Linkedlist:
-    node: int
-    data: object
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-ll = Linkedlist(1,2)
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node:
+                current = current.next_node
+            current.next_node = new_node
+
+    def __repr__(self):
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next_node
+        return " -> ".join(result)
+
+
+ll = LinkedList()
+ll.append(3)
+ll.append(5)
+ll.append(2)
+ll.append(7)
 print(ll)
